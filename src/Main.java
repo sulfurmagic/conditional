be760bb32ead964c029c0a66c6fa5dcfffbdeac1
sulfurmagic <johnaul81@gmail.com>
2024-02-15 21:04:39 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int price = 12_003;
+        int rub = 20;
+
+        int bonus = price / rub;
+
+        System.out.println(bonus);
     }
 }
